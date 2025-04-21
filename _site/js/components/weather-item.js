bc3d6360fcedae6de96b7afccbcc15ffb813a912
Tiago Villaca <60266341+villaca13@users.noreply.github.com>
2025-04-21 23:28:35 +0100

    changes 21/04/2024 23:29:00

--- _site/js/components/weather-item.js
+++ _site/js/components/weather-item.js
@@ -1,23 +1,22 @@
-window.weatherProject.components.createWeatherItem = (city) => {
+window.weatherProject.components.createWeatherItem = (city, cityDaily) => {
     return `
     <div class="column is-4">
-      <section class="card has-text-centered" >
+      <section class="card has-text-centered is-ce " >
         <header class="card-header">
          <a href="/cityFocus/?city=${city}" class="card-header-title is-size-4 is-centered">
-            ${city.replace(/_/g, " ")} 
-          
+            ${city.replace(/_/g, " ")}          
           </a>
         </header>
 
-        <div class="card-image">
-          <figure class="image">
-            <img src="#" alt="Image">
+        <div class="card-image ">
+          <figure class="image is-128x128 is-inline-block">	
+            <img src="images/${cityDaily.daily.weather_code[0]}.png" alt="Image">
           </figure>
         </div>
 
-        <footer class="columns card-footer">
-          <p class="column is-6"> 12C </p>
-          <p class="column is-6"> 12C </p>
+        <footer class="card-footer">
+          <p class="card-footer-item"> ${cityDaily.daily.temperature_2m_min[0]} ${cityDaily.daily_units.temperature_2m_min} </p>
+          <p class="card-footer-item"> ${cityDaily.daily.temperature_2m_max[0]} ${cityDaily.daily_units.temperature_2m_max} </p>
         </footer>
       </section>
     </div>`
